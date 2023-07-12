9a11b8f5b5e5cae5613261545a15059f46b57a1f
Vicky John <vicky.d.john@email.com>
2023-07-12 12:10:36 -0700

    modified to use SQL

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -142,7 +142,7 @@ const getAllProperties = function(options, limit = 10) {
     queryParams.push(options.minimum_rating);
     queryStr += `${queryParams.length > 1 ? 'AND' : 'WHERE'} property_reviews.rating >= $${queryParams.length} `;
   }
-  
+
   queryParams.push(limit);
   queryStr +=
     `
@@ -161,11 +161,36 @@ const getAllProperties = function(options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = (options, limit = 10) => {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = (property) => {
+  const {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  } = property;
+
+  return pool
+    .query(`
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;
+    `, [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms])
+    .then(result => {
+      return result.rows[0]; // Return the saved property object
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
