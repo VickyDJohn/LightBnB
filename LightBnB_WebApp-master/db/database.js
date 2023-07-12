63b49344632b3de1d94e146c51fc6fab80454b83
Vicky John <vicky.d.john@email.com>
2023-07-12 10:10:08 -0700

    modified getAllReservations to use SQL

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -85,7 +85,24 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.id, properties.title as title, properties.cost_per_night, start_date, AVG(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON properties.id = property_id
+    JOIN users ON guest_id = users.id
+    JOIN property_reviews ON reservations.guest_id = property_reviews.guest_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, title, properties.cost_per_night, start_date
+    ORDER BY start_date
+    LIMIT $2;
+  `, [guest_id, limit = 10])
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
